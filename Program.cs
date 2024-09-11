fc386ac898f90c8f13ef3901bbf0d094cceb2710
”Sn1peRzGG” <afly367@gmail.com>
2024-09-11 16:58:47 +0300

    init commit

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using Spectre.Console;
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static List<int> randomNumbers = new List<int>();
+
+    static void Main()
+    {
+        GenerateRandomNumbers();
+
+        AnsiConsole.MarkupLine("[yellow]Generated Numbers:[/]");
+        AnsiConsole.MarkupLine(string.Join(", ", randomNumbers));
+
+        int minRange = AnsiConsole.Ask<int>("Enter the minimum value of range:");
+        int maxRange = AnsiConsole.Ask<int>("Enter the maximum value of range:");
+        int sequenceLength = AnsiConsole.Ask<int>("Enter the length of sequences:");
+
+        Dictionary<int, int> validSequences = new Dictionary<int, int>();
+
+        for (int i = 0; i <= randomNumbers.Count - sequenceLength; i++)
+        {
+            int sum = 0;
+            for (int j = 0; j < sequenceLength; j++)
+            {
+                sum += randomNumbers[i + j];
+            }
+
+            if (sum >= minRange && sum <= maxRange)
+            {
+                validSequences[i] = sum;
+            }
+        }
+
+        if (validSequences.Count > 0)
+        {
+            AnsiConsole.MarkupLine("\n[green]Valid sequences:[/]");
+            foreach (var sequence in validSequences)
+            {
+                AnsiConsole.MarkupLine($"[yellow]Index: {sequence.Key}, Sum: {sequence.Value}[/]");
+            }
+        }
+        else
+        {
+            AnsiConsole.MarkupLine("[red]No valid sequences found.[/]");
+        }
+    }
+
+    static void GenerateRandomNumbers()
+    {
+        Random rand = new Random();
+        for (int i = 0; i < 100; i++)
+        {
+            randomNumbers.Add(rand.Next(100, 901));
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Numbers")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Numbers")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("aa7ee2a0-bd7c-4e73-aff4-2c52f013223d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
